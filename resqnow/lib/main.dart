7bdc524d17152cda63907a0a4e315e8c8ae5ef17
Archana-in-git <archanavs2911@gmail.com>
2025-07-20 11:10:00 +0530

    Intial folder structure

---
+++ resqnow/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ResQNow',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ResQNow Home')),
+      body: const Center(child: Text('Firebase Initialized Successfully!')),
+    );
+  }
+}
