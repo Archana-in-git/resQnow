668cff979ccc4e68922b28b9b46705b357cf37c1
Archana-in-git <archanavs2911@gmail.com>
2025-07-27 23:14:11 +0530

    files updated

--- resqnow/lib/features/presentation/navigation/app_router.dart
+++ resqnow/lib/features/presentation/navigation/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart';
 import 'package:resqnow/features/emergency/presentation/pages/emergency_page.dart';
+import 'package:resqnow/features/home/presentation/pages/home_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: '/emergency',
+    initialLocation: '/home',
     routes: [
+      GoRoute(path: '/home', builder: (context, state) => const HomePage()),
       GoRoute(
         path: '/emergency',
         builder: (context, state) => const EmergencyPage(),
