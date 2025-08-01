8cb47aca59f2ec50af09159f53e34cb0e1a809bb
Archana-in-git <archanavs2911@gmail.com>
2025-08-01 12:55:12 +0530

    Files added

--- resqnow/lib/features/presentation/navigation/app_router.dart
+++ resqnow/lib/features/presentation/navigation/app_router.dart
@@ -1,7 +1,9 @@
-import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:resqnow/features/emergency/presentation/pages/emergency_page.dart';
 import 'package:resqnow/features/home/presentation/pages/home_page.dart';
+import 'package:resqnow/features/condition_categories/presentation/pages/category_list_page.dart';
+import 'package:resqnow/features/medical_conditions/presentation/pages/condition_detail_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
@@ -11,6 +13,17 @@ class AppRouter {
       GoRoute(
         path: '/emergency',
         builder: (context, state) => const EmergencyPage(),
+      ),
+      GoRoute(
+        path: '/categories',
+        builder: (context, state) => const CategoryListPage(),
+      ),
+      GoRoute(
+        path: '/condition/:id',
+        builder: (context, state) {
+          final conditionId = state.params['id']!;
+          return ConditionDetailPage(conditionId: conditionId);
+        },
       ),
     ],
   );
